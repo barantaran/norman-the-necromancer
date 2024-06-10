645dbb397f81ce04160490df91c5a903d2a547ff
Dmitri Yourchev <d.yurchev@yandex.ru>
2024-06-10 16:09:39 +0600

    chore: port to extension

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig, Plugin } from "vite";
 
 export default defineConfig({
-  plugins: [singleFile()],
   base: "./",
   build: {
     polyfillModulePreload: false,
     reportCompressedSize: false,
     assetsInlineLimit: 0,
     minify: "terser",
+    sourcemap: true,
     terserOptions: {
       compress: {
         unsafe_arrows: true,
@@ -30,22 +30,4 @@ export default defineConfig({
   },
 });
 
-function singleFile(): Plugin {
-  return {
-    name: "vite:single-file",
-    enforce: "post",
-    generateBundle(options, bundle) {
-      let html = bundle["index.html"] as any;
-      let js = bundle["index.js"] as any;
 
-      if (html.type === "asset") {
-        html.source = html.source
-          .replace(/<script.*<\/script>/, "")
-          .replace("</body>", () => `<script>${js.code}</script>`)
-          .replace(/\n+/g, "");
-      }
-
-      delete bundle[js.fileName];
-    }
-  };
-}
