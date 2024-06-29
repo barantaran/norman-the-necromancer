2cf8185395211b140b31dc36ad16354cdb785e56
Dmitri Yourchev <d.yurchev@yandex.ru>
2024-06-29 12:11:13 +0600

    feat: launch game after installation

--- public/background.js
+++ public/background.js
@@ -1,3 +1,9 @@
 chrome.action.onClicked.addListener(tab => {
   chrome.tabs.create({ url: chrome.runtime.getURL('index.html') });
 });
+
+chrome.runtime.onInstalled.addListener(function(details){
+  if(details.reason == "install"){
+    chrome.tabs.create({ url: chrome.runtime.getURL('index.html') });
+  }
+});
